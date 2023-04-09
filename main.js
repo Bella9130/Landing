5af3a2d48a86730324d8a24737ec5f381790752b
Bella <salguero.cano.bellla@gmail.com>
2023-04-09 17:39:44 -0600

    commit fix

---
+++ main.js
@@ -0,0 +1,20 @@
+const imgMenuLogo = document.querySelector('.imagen__toggle');
+const inacImgMenu = document.querySelector('.menu__logo');
+
+
+imgMenuLogo.addEventListener('click', toggleMenu);
+
+
+
+function toggleMenu(){
+    inacImgMenu.classList.toggle("inactive");
+};
+
+
+
+
+
+
+
+
+
